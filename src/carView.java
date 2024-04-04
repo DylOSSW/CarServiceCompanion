3e7ef4d22f7edc3747e7cbffc17c7de119f03eb2
D21124331 <D21124331@TU-4GC8BK3.ict.ad.dit.ie>
2024-04-04 13:58:26 +0100

    scrollview autopopulating with car cards

--- src/carView.java
+++ src/carView.java
@@ -11,6 +11,14 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 
+import javax.swing.Box;
+
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.BorderFactory;
+
+
 public class carView extends javax.swing.JFrame {
     private SimpleDBConnect dbConnect;
 
@@ -23,85 +31,107 @@ public class carView extends javax.swing.JFrame {
         populateCarsPanel();
     }
     
-    private void populateCarsPanel() {
-        ResultSet carsResultSet = dbConnect.getAllCars();
+private void populateCarsPanel() {
+    ResultSet carsResultSet = dbConnect.getAllCars();
+    Dimension screenSize = jScrollPane1.getViewport().getExtentSize(); // Get the visible area dimension
+    int cardWidth = screenSize.width - 30; // Subtract a margin value from the total width
 
-        containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.Y_AXIS));
+    // Assuming jPanel1 is already set with a BoxLayout in the GUI designer
+    jPanel1.setLayout(new BoxLayout(jPanel1, BoxLayout.Y_AXIS));
 
-        try {
-            while (carsResultSet.next()) {
-                JPanel carPanel = new JPanel();
-                
-                // Assuming your Cars table has columns: Id, Make, Model, Year, Price
-                idLabel = new JLabel("carID: " + carsResultSet.getString("carID"));
-                typeLabel = new JLabel("carType: " + carsResultSet.getString("carType"));
+    try {
+        while (carsResultSet.next()) {
+            // Panel for each car card with some padding
+            JPanel carPanel = new JPanel();
+            carPanel.setLayout(new BoxLayout(carPanel, BoxLayout.Y_AXIS));
+            carPanel.setBorder(BorderFactory.createCompoundBorder(
+                    BorderFactory.createLineBorder(Color.BLACK, 1), // Outer line border
+                    BorderFactory.createEmptyBorder(10, 10, 10, 10))); // Padding border
+            carPanel.setBackground(Color.WHITE);
 
-                carPanel.add(idLabel);
-                carPanel.add(typeLabel);
-                // Add more components to the panel
+            // Set maximum and preferred size for larger cards
+            carPanel.setMaximumSize(new Dimension(cardWidth, 150));
+            carPanel.setPreferredSize(new Dimension(cardWidth, 150));
 
-                containerPanel.add(carPanel);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+            // Create new labels for each car's information
+            JLabel carIdLabel = new JLabel("carID: " + carsResultSet.getString("carID"));
+            JLabel makeLabel = new JLabel("Make: " + carsResultSet.getString("Make"));
+
+            // Add labels to the car panel
+            carPanel.add(carIdLabel);
+            carPanel.add(makeLabel);
+
+            // Add the car panel as a 'card' to jPanel1
+            jPanel1.add(carPanel);
+
+            // Add a rigid area for spacing between cards
+            jPanel1.add(Box.createRigidArea(new Dimension(0, 20)));
         }
+    } catch (SQLException e) {
+        e.printStackTrace();
     }
+
+    // Revalidate and repaint jPanel1 to update the GUI
+    jPanel1.revalidate();
+    jPanel1.repaint();
+}
+
+
+
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         containerPanel = new javax.swing.JPanel();
-        typeLabel = new javax.swing.JLabel();
-        idLabel = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jScrollPane1.setBackground(new java.awt.Color(255, 255, 204));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 204));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 302, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 305, Short.MAX_VALUE)
+        );
+
+        jScrollPane1.setViewportView(jPanel1);
 
         javax.swing.GroupLayout containerPanelLayout = new javax.swing.GroupLayout(containerPanel);
         containerPanel.setLayout(containerPanelLayout);
         containerPanelLayout.setHorizontalGroup(
             containerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 519, Short.MAX_VALUE)
-            .addGroup(containerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(containerPanelLayout.createSequentialGroup()
-                    .addGap(50, 50, 50)
-                    .addComponent(typeLabel)
-                    .addContainerGap(469, Short.MAX_VALUE)))
-            .addGroup(containerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(containerPanelLayout.createSequentialGroup()
-                    .addGap(50, 50, 50)
-                    .addComponent(idLabel)
-                    .addContainerGap(469, Short.MAX_VALUE)))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, containerPanelLayout.createSequentialGroup()
+                .addContainerGap(372, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(61, 61, 61))
         );
         containerPanelLayout.setVerticalGroup(
             containerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 320, Short.MAX_VALUE)
-            .addGroup(containerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(containerPanelLayout.createSequentialGroup()
-                    .addGap(50, 50, 50)
-                    .addComponent(typeLabel)
-                    .addContainerGap(270, Short.MAX_VALUE)))
-            .addGroup(containerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(containerPanelLayout.createSequentialGroup()
-                    .addGap(50, 50, 50)
-                    .addComponent(idLabel)
-                    .addContainerGap(270, Short.MAX_VALUE)))
+            .addGroup(containerPanelLayout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 307, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(79, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(54, 54, 54)
-                .addComponent(containerPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(164, Short.MAX_VALUE))
+            .addComponent(containerPanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(42, 42, 42)
-                .addComponent(containerPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(45, Short.MAX_VALUE))
+            .addComponent(containerPanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -144,7 +174,7 @@ public class carView extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel containerPanel;
-    private javax.swing.JLabel idLabel;
-    private javax.swing.JLabel typeLabel;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
