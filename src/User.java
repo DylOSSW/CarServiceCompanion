841ae4cdbb8ac55c43a7b6116fef7b963ecfb070
D21124331 <D21124331@TU-4GC8BK3.ict.ad.dit.ie>
2024-04-27 22:54:46 +0100

    User class

--- src/User.java
+++ src/User.java
@@ -1,7 +1,25 @@
 public class User {
     private String email;
-    private String name;
-    // Other user properties
+    private String forename;
+    private String surname;
 
-    // Constructor, getters, and setters
+    // Update the constructor to accept and initialize the surname as well
+    public User(String email, String forename, String surname) {
+        this.email = email;
+        this.forename = forename;
+        this.surname = surname;
+    }
+
+    // Getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
 }
